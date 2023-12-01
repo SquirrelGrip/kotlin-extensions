40203c9901319b67d2d09a951f464885ec50ea81
Adrian Richter <adrian.richter@gmail.com>
2023-12-01 21:53:13 +0800

    Clean up the pom.xml

--- kotlin-extensions-jvm/src/test/kotlin/com/github/squirrelgrip/util/InitOncePropertyTest.kt
+++ kotlin-extensions-jvm/src/test/kotlin/com/github/squirrelgrip/util/InitOncePropertyTest.kt
@@ -5,8 +5,6 @@ import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Test
 import kotlin.properties.ReadWriteProperty
 
-inline fun <reified T> initOnce(): ReadWriteProperty<Any, T> = InitOnceProperty()
-
 class InitOncePropertyTest {
 
     var property: String by initOnce()
